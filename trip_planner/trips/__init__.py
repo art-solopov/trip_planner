420ea452a5bc0dca8b3e49034f12d58cded6db26
Artemiy Solopov <art-solopov@yandex.ru>
2020-01-16 21:45:08 +0300

    Trip create/update

--- trip_planner/trips/__init__.py
+++ trip_planner/trips/__init__.py
@@ -1,6 +1,9 @@
 from itertools import groupby
 from operator import attrgetter
 from flask import Blueprint, g, render_template, redirect, url_for
+from flask.views import MethodView as View
+from sqlalchemy.exc import IntegrityError
+import psycopg2.errorcodes as pgerrorcodes
 
 from .. import db
 from ..shared import user_required
@@ -33,15 +36,77 @@ def show(slug):
                            map_url=g.map_data.map_url)
 
 
-@trips.route("/new", methods=('GET', 'POST'))
-@user_required
-def new():
-    form = TripForm()
-    if form.validate_on_submit():
-        model = Trip(author_id=g.user.id)
-        form.populate_obj(model)
-        db.session.add(model)
-        db.session.commit()
-        return redirect(url_for('.index'))
-    print(form.errors)
-    return render_template('form.html', form=form, submit_text='Create trip')
+class TripCUView(View):
+    methods = ('GET', 'POST')
+    decorators = [user_required]
+    title = 'Trip action'
+    submit_text = 'Save'
+
+    def dispatch_request(self, *args, **kwargs):
+        self.model = self._instant_model()
+        return super().dispatch_request(*args, **kwargs)
+
+    def get(self):
+        self.form = self._build_form()
+        return self._default_render()
+
+    def post(self):
+        self.form = TripForm()
+        if self.form.validate():
+            try:
+                self.form.populate_obj(self.model)
+                db.session.add(self.model)
+                db.session.commit()
+                return redirect(url_for('.index'))
+            except IntegrityError as e:
+                db.session.rollback()
+                if (e.orig.pgcode == pgerrorcodes.UNIQUE_VIOLATION):
+                    self.form.errors.setdefault('slug', [])
+                    self.form.errors['slug'].append('Already exists')
+                    return self._default_render()
+                else:
+                    raise e
+        return self._default_render()
+
+    def _build_form(self):
+        raise NotImplementedError
+
+    def _instant_model(self) -> Trip:
+        raise NotImplementedError
+
+    def _default_render(self):
+        return render_template('form.html', form=self.form,
+                               title=self.title,
+                               submit_text=self.submit_text)
+
+
+class CreateTripView(TripCUView):
+    title = 'Create trip'
+
+    def _build_form(self):
+        return TripForm()
+
+    def _instant_model(self):
+        return Trip(author=g.user)
+
+
+class UpdateTripView(TripCUView):
+    @property
+    def title(self):
+        return f"Updating {self.model.name}"
+
+    def dispatch_request(self, slug):
+        self.slug = slug
+        return super().dispatch_request()
+
+    def _build_form(self):
+        return TripForm(obj=self.model)
+
+    def _instant_model(self):
+        return Trip.query\
+                   .filter_by(slug=self.slug, author=g.user)\
+                   .first_or_404()
+
+
+trips.add_url_rule('/new', view_func=CreateTripView.as_view('new'))
+trips.add_url_rule('/<slug>/update', view_func=UpdateTripView.as_view('update'))
