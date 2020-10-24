56eacee1079a8c298e3363b3ffadcb5d89898a46
Artemiy Solopov <art-solopov@yandex.ru>
2020-10-24 22:04:56 +0300

    Replaced parcel with webpack with tree shaking

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        map: './assets/js/map.js',
+        point_form: './assets/js/point_form.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'trip_planner/static/js'),
+        filename: '[name].js',
+        publicPath: '/static/js/'
+    },
+    devtool: 'cheap-module-source-map',
+    optimization: {
+        usedExports: true
+    }
+}
