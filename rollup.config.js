9c46b5d13d30714b84115cfe3a67f088bf9262e0
Artemiy Solopov <art-solopov@yandex.ru>
2021-08-06 00:06:32 +0300

    Compiling styles with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,19 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import manifest from 'rollup-plugin-output-manifest'
+import styles from 'rollup-plugin-styles'
 
 export default {
     input: {
         point_form: 'assets/js/point_form.js',
         map: 'assets/js/map.js',
-        shared_app: 'assets/js/shared_app.js'
+        shared_app: 'assets/js/shared_app.js',
+        app: 'assets/css/app.css'
     },
     output: {
         dir: 'trip_planner/static/js',
-        // entryFileNames: '[name]-[hash].js',
+        entryFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash][extname]',
         format: 'es',
         sourcemap: true
     },
@@ -22,6 +25,8 @@ export default {
         resolve({jsnext: true, preferBuiltins: true, browser: true}),
         commonjs(),
         json(),
-        manifest(),
+        // postcss({extract: path.resolve('trip_planner/static/css/app-[hash].css')}),
+        styles({mode: 'extract'}),
+        manifest({nameWithExt: false}),
     ]
 };
