7b2a3c8c0aabfe2912b692fef2db6884bdc1c6ec
Artemiy Solopov <art-solopov@yandex.ru>
2022-07-09 00:04:24 +0300

    First apps: trips, assets, locations

--- trip_planner/settings.py
+++ trip_planner/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'assets',
+    'trips',
+    'locations'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,8 @@ WSGI_APPLICATION = 'trip_planner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trip_planner_v2'
     }
 }
 
