754f39a703da4984a295568b10edec96abf45ee9
Artemiy Solopov <art-solopov@yandex.ru>
2022-12-30 23:52:36 +0200

    Replaced collapsibles/collapsible maps with Bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,5 @@ import * as bootstrap from 'bootstrap'
 import '../css/app.scss'
 import { initComponent } from './utils'
 import Flash from './components/flash'
-import Collapsible from './components/collapsible'
 
 initComponent(Flash)
-initComponent(Collapsible)
