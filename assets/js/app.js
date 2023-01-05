2f1dfd0bf46718a77b1f28604dd0e3f8336d9327
Artemiy Solopov <art-solopov@yandex.ru>
2023-01-05 17:51:44 +0200

    Various components

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,4 @@
 import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap
 
 import '../css/app.scss'
-import { initComponent } from './utils'
-import Flash from './components/flash'
-
-initComponent(Flash)
