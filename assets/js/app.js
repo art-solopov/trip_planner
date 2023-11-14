b1f59f6a498669ca004c0114f2666efdc7ccf117
Artemiy Solopov <art-solopov@yandex.ru>
2023-11-14 22:36:24 +0200

    Removed Bootstrap, added PureCSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
-import './bootstrap_custom'
+// import './bootstrap_custom'
 import { initComponent } from './utils'
 
 import Dialog from './components/dialog'
-import Tooltip from './components/tooltip'
+// import Tooltip from './components/tooltip'
 import RepeatableField from './components/repeatable_field'
 
 import '../css/app.scss'
