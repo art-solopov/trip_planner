c2f14ce5781b33094de5c28801e775011bf2ae11
Artemiy Solopov <art-solopov@yandex.ru>
2023-11-18 14:36:46 +0200

    Removed Bootstrap; added Plume

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,13 @@
-import './bootstrap_custom'
+// import './bootstrap_custom'
 import { initComponent } from './utils'
 
 import Dialog from './components/dialog'
-import Tooltip from './components/tooltip'
+// import Tooltip from './components/tooltip'
 import RepeatableField from './components/repeatable_field'
 
+import 'plume-css/lib/plume-all.css'
 import '../css/app.scss'
 
 initComponent(Dialog)
-initComponent(Tooltip)
+// initComponent(Tooltip)
 initComponent(RepeatableField)
