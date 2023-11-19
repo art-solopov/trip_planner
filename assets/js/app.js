afd976f5ee7f0be430d714673f48add5bc7f314c
Artemiy Solopov <art-solopov@yandex.ru>
2023-11-19 18:40:53 +0200

    Reconfiguration (more local plume classes)

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,6 @@ import Dialog from './components/dialog'
 // import Tooltip from './components/tooltip'
 import RepeatableField from './components/repeatable_field'
 
-import 'plume-css/lib/plume-all.css'
 import '../css/app.scss'
 
 initComponent(Dialog)
