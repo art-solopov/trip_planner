36eba0f42fbad1c689331a6419307181c60b2030
Artemiy Solopov <a.solopov.001@gmail.com>
2026-02-19 01:23:31 +0200

    Continuing to HTMX-ify point creation/editing

--- assets/js/trip_show.js
+++ assets/js/trip_show.js
@@ -2,5 +2,7 @@ import { createApp } from './utils'
 
 import Map from './components/map'
 import TripShow from './components/trip_show'
+import Geocode from './components/geocode'
+import PointForm from './components/point_form'
 
-createApp(Map, TripShow)
+createApp(Map, TripShow, Geocode, PointForm)
